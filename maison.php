c5ea76f2699970f3b0cd06c78044c39a2a4b34e6
molka6 <molkabenameur6@gmail.com>
2021-12-11 00:09:18 +0100

    11/12

--- maison.php
+++ maison.php
@@ -16,98 +16,7 @@
   <link href="style/css/style.css" rel="stylesheet"> 
 </head>
 <body>
-<?php include 'Connexion/connexion.php';?>
   <div class="click-closed"></div>
-  <div class="box-collapse">
-    <div class="title-box-d">
-      <h3 class="title-d">Search Property</h3>
-    </div>
-    <span class="close-box-collapse right-boxed bi bi-x"></span>
-    <div class="box-collapse-wrap form">
-      <form class="form-a">
-        <div class="row">
-          <div class="col-md-12 mb-2">
-            <div class="form-group">
-              <label class="pb-2" for="Type">Keyword</label>
-              <input type="text" class="form-control form-control-lg form-control-a" placeholder="Keyword">
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="Type">Type</label>
-              <select class="form-control form-select form-control-a" id="Type">
-                <option>All Type</option>
-                <option>For Rent</option>
-                <option>For Sale</option>
-                <option>Open House</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="city">City</label>
-              <select class="form-control form-select form-control-a" id="city">
-                <option>All City</option>
-                <option>Alabama</option>
-                <option>Arizona</option>
-                <option>California</option>
-                <option>Colorado</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="bedrooms">Bedrooms</label>
-              <select class="form-control form-select form-control-a" id="bedrooms">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="garages">Garages</label>
-              <select class="form-control form-select form-control-a" id="garages">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-                <option>04</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="bathrooms">Bathrooms</label>
-              <select class="form-control form-select form-control-a" id="bathrooms">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group mt-3">
-              <label class="pb-2" for="price">Min Price</label>
-              <select class="form-control form-select form-control-a" id="price">
-                <option>Unlimite</option>
-                <option>$50,000</option>
-                <option>$100,000</option>
-                <option>$150,000</option>
-                <option>$200,000</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <button type="submit" class="btn btn-b">Search Property</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
   <?php include 'navbar.php';?>
   <main id="main">
     <section class="intro-single">
@@ -133,6 +42,19 @@
         </div>
       </div>
     </section>
+    <?php 
+if(isset($_GET['page']) && !empty($_GET['page'])){
+  $currentPage = (int) strip_tags($_GET['page']);
+}else{
+  $currentPage = 1;
+}
+require_once 'Connexion/connexion.php' ;
+$sql ='SELECT COUNT(*) AS nb_articles FROM `maison` ' ;
+$query =  $mysqlClient->prepare($sql);
+$query-> execute(); 
+$result = $query->fetch();
+$nbArticles = (int) $result['nb_articles'];
+?>
     <section class="property-grid grid">
       <div class="container">
         <div class="row">
@@ -146,19 +68,23 @@
                   <option value="3">For Sale</option>
                 </select>
               </form>
+              <p style="color : black ; float: left ; margin-left:3% ;" >  <?php echo $nbArticles;  ?>  Biens </p>
             </div>
-          </div>
+          </div>          
 <!-- ----------------------------------------------------------------------------------- -->
 <?php
-$sqlQuery = 'SELECT * FROM maison';
-$recipesStatement = $mysqlClient->prepare($sqlQuery);
-$recipesStatement->execute();
-$recipes = $recipesStatement->fetchAll();
+$parPage = 12;
+$pages = ceil($nbArticles / $parPage);
+$premier = ($currentPage * $parPage) - $parPage;
+$sql = 'SELECT * FROM `maison` ORDER BY `Date_Creation` DESC LIMIT :premier, :parpage;';
+$query =  $mysqlClient->prepare($sql);
+$query->bindValue(':premier', $premier, PDO::PARAM_INT);
+$query->bindValue(':parpage', $parPage, PDO::PARAM_INT);
+$query-> execute(); 
+$recipes = $query->fetchAll(PDO::FETCH_ASSOC);
+$recipesStatement = $mysqlClient->prepare($sql);
 foreach ($recipes as $key => $recipe) {
 ?>
-
-<?php  if($key < 8) {
-  ?>
     <div class="col-md-3">
             <div class="card-box-a card-shadow">
               <div class="img-box-a">
@@ -169,7 +95,8 @@ foreach ($recipes as $key => $recipe) {
                   <div class="card-header-a">
                     <h2 class="card-title-a">
                       <a href="#">204 Mount
-                        <br /> <?php echo $recipe['Titre']; ?> </a>
+                        <br /> <?php echo $recipe['Titre']; ?>
+                       </a>
                     </h2>
                   </div>
                   <div class="card-body-a">
@@ -201,36 +128,38 @@ foreach ($recipes as $key => $recipe) {
             </div>
           </div>
 <?php
-}}
+}
+require_once('Connexion/close.php');
 ?>
         </div>
         <div class="row">
           <div class="col-sm-12">
             <nav class="pagination-a">
               <ul class="pagination justify-content-end">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">
+                <li class="page-item   <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                  <a class="page-link" href="maison.php?page=<?= $currentPage - 1 ?>" tabindex="-1">
                     <span class="bi bi-chevron-left"></span>
                   </a>
                 </li>
-                <li class="page-item">
-                  <a class="page-link" href="#">1</a>
-                </li>
-                <li class="page-item active">
-                  <a class="page-link" href="#">2</a>
-                </li>
-                <li class="page-item">
-                  <a class="page-link" href="#">3</a>
-                </li>
-                <li class="page-item next">
-                  <a class="page-link" href="#">
+               
+               
+                <?php for($page = 1; $page <= $pages; $page++): ?>
+                    <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                        <a href="maison.php?page<?= $page ?>" class="page-link"><?= $page ?></a>
+                    </li>
+                <?php endfor ?>
+                
+
+                <li class="page-item next <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                  <a class="page-link" href="maison.php?page=<?= $currentPage + 1 ?>">
                     <span class="bi bi-chevron-right"></span>
                   </a>
                 </li>
+
               </ul>
             </nav>
           </div>
-        </div>
+        </div> 
       </div>
     </section>
   </main>
