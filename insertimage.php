5a32d75a5ea622a328e6ae2e8d9e14649dbd574a
molka6 <molkabenameur6@gmail.com>
2021-12-25 21:04:39 +0100

    25/12/2021

--- insertimage.php
+++ insertimage.php
@@ -2,86 +2,70 @@
 
 require_once 'Connexion/connexion.php' ;
 
-
-var_dump($_POST);
-var_dump($_FILES);
-
-if(isset($_FILES['file'])){
-    $tmpName = $_FILES['file']['tmp_name'];
-    $name = $_FILES['file']['name'];
-    $size = $_FILES['file']['size'];
-    $error = $_FILES['file']['error'];
-
-    $tabExtension = explode('.', $name);
-    $extension = strtolower(end($tabExtension));
-    //Tableau des extensions que l'on accepte
-    $extensions = ['jpg', 'png', 'jpeg', 'gif'];
-    //Taille max que l'on accepte
-    $maxSize = 400000;
+                        $req_membres = $mysqlClient->prepare("INSERT INTO  appartement(Titre,Superficie) VALUES(?,?)");
+                        $titre = htmlspecialchars($_POST['titre']);
+                        $superficie = htmlspecialchars($_POST['supert']);
+                        $req_membres->execute(array($titre,$superficie));                                     
+                        $idm = $mysqlClient->lastInsertId();    
 
 
-
-$req_membres = $mysqlClient->prepare("INSERT INTO  appartement(Titre,Superficie) VALUES(?,?)");
-$titre = htmlspecialchars($_POST['titre']);
-$superficie = htmlspecialchars($_POST['supert']);
-$req_membres->execute(array($titre,$superficie));                                     
-$idm = $mysqlClient->lastInsertId();    
+                        $req_option= $mysqlClient->prepare("INSERT INTO option_appartement(appartement_id,Balcon,Climatisation) VALUES(?,?,?)");
+                        $Balcon= htmlspecialchars($_POST['Balcon']);
+                        $Climatisation= htmlspecialchars($_POST['Climatisation']);
+                        $req_option->execute(array($idm,$Balcon,$Climatisation)); 
 
 
-
-
-
-    if(in_array($extension, $extensions) && $size <= $maxSize && $error == 0){
-        $uniqueName = uniqid('', true);
-        //uniqid génère quelque chose comme ca : 5f586bf96dcd38.73540086
-        $file = $uniqueName.".".$extension;
-        //$file = 5f586bf96dcd38.73540086.jpg
-        move_uploaded_file($tmpName, './Images/'.$file);
-
-        $req = $mysqlClient->prepare('INSERT INTO image_appartement (titre,appartement_id) VALUES (?,?)');
-
-
-        $req->execute(array($file,$idm));
-        echo "Image enregistrée";
-
- 
-    }
-    else{
-        echo "Une erreur est survenue";
-    }
-
-}
-
-// $file = $uniqueName.".".$extension;
-// //$file = 5f586bf96dcd38.73540086.jpg
-// move_uploaded_file($tmpName, './Images/'.$file);
-// $req = $mysqlClient->prepare('INSERT INTO image (titre) VALUES (?)');
-// $req->execute([$file]);
-// echo "Image enregistrée";
-
-
-
+                        $tmpName = $_FILES['file']['tmp_name'];
+                        $name= $_FILES['file']['name'];
+                        $size = $_FILES['file']['size'];
+                        $error = $_FILES['file']['error'];
+                        for($i=0; $i<count($name); $i++) {
+                            $tabExtension[$i] = explode('.', $name[$i]);
+                            $extension[$i] = strtolower(end($tabExtension[$i]));
+                            echo "extention =". $extension[$i];
+                        }
+                        $nbfichiersEnvoyes = count($tmpName);
+                        for($i=0; $i<$nbfichiersEnvoyes; $i++) {
+                            $tabExtension[$i]= explode('.', $name[$i]);
+                            $extension[$i] = strtolower(end($tabExtension[$i]));
+                            $uniqueName = uniqid('', true);
+                            $file[$i] = $uniqueName.".".$extension[$i];
+                            move_uploaded_file($tmpName[$i], './Images/'.$file[$i]);
+                            $req = $mysqlClient->prepare('INSERT INTO image_appartement (titre,appartement_id) VALUES (?,?)');
+                            $req->execute(array($file[$i],$idm));
+                            echo "Image enregistrée";
+                        }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-
 </head>
 <body>
-
-
-
-
     <form action="insertimage.php" method="POST" enctype="multipart/form-data">
         <label for="file">Fichier</label>
-        <input type="file" name="file">
+        <input type="file" name="file[]" multiple>
+        <label for="file">titer</label>
         <input type="text" name="titre">
+        <label for="file">superficie</label>
         <input type="text" name="supert">
 
+        
+        <div> 
+            <input type="checkbox" id="Balcon" name="Balcon">
+            <label for="Balcon">Balcon</label>
+        </div>
+
+
+        <div>
+            <input type="checkbox" id="Climatisation" name="Climatisation">
+            <label for="Climatisation">Climatisation</label>
+        </div>
+
         <button type="submit">Enregistrer</button>
+
+
     </form>
 
     
